83b321359b6e7d53095a570b02de8d641f34c9d2
Vaibhav sisodiya <91018916+Vaib215@users.noreply.github.com>
2022-10-10 01:31:50 +0530

    Updated COrs origin

--- server/index.js
+++ server/index.js
@@ -21,18 +21,9 @@ const connect = async () => {
 
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: "https://notes-by-vaib.vercel.app/",
     credentials: true
 }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === 'OPTIONS') {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRoutes);
