bf3c58269f24d54cea422ebdc3b6950d1cc83a63
Vaibhav sisodiya <91018916+Vaib215@users.noreply.github.com>
2022-10-10 01:20:40 +0530

    Improved cors

--- server/index.js
+++ server/index.js
@@ -24,6 +24,15 @@ app.use(cors({
     origin: "*",
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRoutes);
